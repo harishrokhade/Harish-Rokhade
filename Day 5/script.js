5579f8d85e29c698d99f1a7455025da8ccc0a033
Hrokhade <rokhadeharish@gmail.com>
2024-11-09 10:58:13 +0530

    Registration Assig

---
+++ Day 5/script.js
@@ -0,0 +1,48 @@
+// console.log("This is Javascript")
+
+
+
+// var name="GMIT"   //Global variable
+// function f1(){
+// console.log("f1" +name)
+// }
+// function f2(){
+//     console.log("f2" +name)
+// }
+
+// function fnName(){      //function called
+//     console.log("called")
+// }
+// fnName(); //fucnction calling
+
+
+// function fnAdd(pat1,pat2){
+//     console.log(pat1+pat2)
+
+// }
+
+// fnAdd(1,2);
+
+
+// {
+//     var a=10;
+//     console.log(a);
+//     let b=20;
+//     console.log(b);
+//     const c=30;
+//     console.log(c);
+// }
+
+var username = "gmit";
+var password = "gmit123";
+
+function fnvalidate() {
+    let inputUsername = document.getElementById("username").value;
+    let inputPassword = document.getElementById("password").value;
+
+    console.log(inputUsername);
+    console.log(inputPassword);
+
+ 
+}
+
